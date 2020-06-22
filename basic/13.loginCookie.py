28de6abcdadc6b66c8e2b7f0271b16c0319cdb53
johnhsq <johnhsq@yahoo.com>
2020-06-22 12:01:24 -0700

    add cookie usage

--- basic/13.loginCookie.py
+++ basic/13.loginCookie.py
@@ -24,3 +24,39 @@ print('Going to profile page...')
 r = requests.get('http://pythonscraping.com/pages/cookies/welcome.php', 
                  cookies=r.cookies)
 print(r.text)
+
+
+print("-"*100)
+# http://www.editthiscookie.com/ is a Chrome extension to help you analyze the site cookie usage
+# Requests library will be unable to handle many cookies produced by modern tracking software, such as Google Analytics, which are set only after the execution of javascripts.
+# Selenium and Chrome WebDrivers have to be used
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(
+    executable_path='drivers/chromedriver', 
+    options=chrome_options)
+driver.get('http://pythonscraping.com')
+driver.implicitly_wait(1)
+# print the cookies
+savedCookies = driver.get_cookies()
+print(savedCookies)
+
+# To manipulate cookies, you can call the delete_cookie(), add_cookie(), and delete_all_cookies() functions. 
+driver2 = webdriver.Chrome(
+    executable_path='drivers/chromedriver',
+    options=chrome_options)
+
+driver2.get('http://pythonscraping.com')
+driver2.delete_all_cookies()
+# use the previously saved cookies for this scraper
+# According to Google Analytics, this second webdriver is now identical to the first one, 
+# and they will be tracked in the same way. 
+# If the first webdriver was logged into a site, the second webdriver will be as well.
+for cookie in savedCookies:
+    driver2.add_cookie(cookie)
+
+driver2.get('http://pythonscraping.com')
+driver.implicitly_wait(1)
+print(driver2.get_cookies())
